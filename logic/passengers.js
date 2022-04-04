d099937704328f8a20fbba0dacfb71991b49cc1c
IvnRmcd <ivn.cox@outlook.com>
2022-04-04 18:50:19 -0400

    create checkFlightCapacity function

--- logic/passengers.js
+++ logic/passengers.js
@@ -1,4 +1,15 @@
 function Passengers() {
 
+ const checkFlightCapacity = (flightCapacity, passengersArray) => {
+   let totalPassengers = passengersArray.reduce((a,b) => a + b)
+
+   if (totalPassengers < flightCapacity){
+     return totalPassengers
+   } else throw new Error ("Number of Passengers exceeds flight Capacity")
+ }
+
+ return {checkFlightCapacity}
 }
 
+
+module.exports = Passengers()
