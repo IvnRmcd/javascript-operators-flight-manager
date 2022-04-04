cf2dc1babcc3d01d88c7210217c186a3312f59a8
IvnRmcd <ivn.cox@outlook.com>
2022-04-04 18:17:38 -0400

    Apply capacity check

--- logic/flights.js
+++ logic/flights.js
@@ -1,6 +1,9 @@
 function Flights() {
   const calculateNumberOfFlights = (numberOfPassengers,flightCapacity ) => {
-
+    if (flightCapacity % numberOfPassengers === 0){
+      return flightCapacity / numberOfPassengers
+    }
+    return flightCapacity / numberOfPassengers + 1
   }
   return {calculateNumberOfFlights}
 }
