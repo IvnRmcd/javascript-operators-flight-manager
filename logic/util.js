0ea5cf01f73034f2702c09bdd433fc98bff4a42f
IvnRmcd <ivn.cox@outlook.com>
2022-04-04 20:22:02 -0400

    Update util.js

--- logic/util.js
+++ logic/util.js
@@ -1,4 +1,14 @@
 function Util() {
 
+  const calculateTotalDistributedPassengers = (distributedObj) => {
+    let totalDistributedPassengers = 0 
+    for (value in distributedObj){
+      totalDistributedPassengers += distributedObj[value]
+    }
+
+    return totalDistributedPassengers
+  }
+
 }
 
+module.exports = Util()
